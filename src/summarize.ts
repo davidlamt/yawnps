7a6c483efccc3c582e74b010cd451485cff52b5c
David Tran <davidlamt@users.noreply.github.com>
2020-12-10 20:42:31 -0800

    Get website content

--- src/summarize.ts
+++ src/summarize.ts
@@ -1,3 +1,15 @@
-export const summarize = ({ url }: { url: string }): void => {
-  console.log(`should summarize ${url}`);
+import fetch from 'node-fetch';
+import * as extractor from 'unfluff';
+
+export const summarize = async ({ url }: { url: string }): Promise<void> => {
+  const content = await getUrlContent(url);
+  console.log(content);
+};
+
+const getUrlContent = async (url: string): Promise<string> => {
+  const response = await fetch(url);
+  const html = await response.text();
+  const data = extractor(html);
+
+  return data.text;
 };
