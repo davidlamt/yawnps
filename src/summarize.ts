56f9d1bee663307492ad3220a9b59debc134ce17
David Tran <davidlamt@users.noreply.github.com>
2020-12-14 19:57:57 -0800

    Handle common errors

--- src/summarize.ts
+++ src/summarize.ts
@@ -2,13 +2,28 @@ import fetch from 'node-fetch';
 import * as extractor from 'unfluff';
 
 export const summarize = async ({ url }: { url: string }): Promise<void> => {
-  const content = await getUrlContent(url);
-  console.log(content);
+  try {
+    const content = await getUrlContent(url);
+    console.log(content);
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 const getUrlContent = async (url: string): Promise<string> => {
-  const response = await fetch(url);
-  const html = await response.text();
+  let html: string;
+
+  try {
+    const response = await fetch(url);
+    html = await response.text();
+  } catch (err) {
+    if (err?.code === 'ENOTFOUND') {
+      throw new Error(`Cannot find ${url}`);
+    }
+
+    throw err;
+  }
+
   const data = extractor(html);
 
   return data.text;
